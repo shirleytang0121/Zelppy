3f516a60953c01acfc4f2d08c1544ab74c40fa1f
Shirley Tang <shirleytang1996@gmail.com>
2021-08-02 02:51:32 -0400

    add store

--- frontend/zelppy.jsx
+++ frontend/zelppy.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from '../frontend/store/store.js'
+import {login} from '../frontend/actions/session_actions'
+
 
 document.addEventListener('DOMContentLoaded',()=>{
+    let store = configureStore();
+    window.store =store;
+    window.login = login;
     const root = document.getElementById('root');
     ReactDOM.render(<h3>zelppy</h3>, root)
 })
