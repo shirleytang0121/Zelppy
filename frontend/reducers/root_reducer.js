b01fea70c7e3e643a64a091ad9914587082fc846
Shirley Tang <shirleytang1996@gmail.com>
2021-08-01 22:44:02 -0400

    add reducers for session

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import sessionReducer from "./session_reducer";
+
+
+const rootReducer = combineReducers({
+    sessionReducer
+});
+
+export default rootReducer;
