423a1f2bd361d422894323a99c69f2b0287f979e
Shirley Tang <shirleytang1996@gmail.com>
2021-08-01 22:56:23 -0400

    add user reducer

---
+++ frontend/reducers/user_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+
+const userReducer = (oldState={}, action)=>{
+    Object.freeze(oldState);
+    let nextState = Object.assign({}, oldState)
+    switch(action.type){
+        case RECEIVE_CURRENT_USER:
+            return nextState[action.user.id] =action.user
+        default:
+            return oldState;
+    }
+}
+
+export default userReducer;
