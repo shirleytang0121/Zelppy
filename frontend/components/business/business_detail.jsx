a5f311b5193f3fdd3ffe27c15771ec6b2268607b
Shirley Tang <shirleytang1996@gmail.com>
2021-08-05 11:58:57 -0400

    fix hours problem

--- frontend/components/business/business_detail.jsx
+++ frontend/components/business/business_detail.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import GreetingContainer from '../greeting/greeting_container'
+import { MdLaunch } from "react-icons/md";
+import { BiPhoneCall } from "react-icons/bi";
 
 class BusinessDetail extends React.Component{
 
     componentDidMount(){
         this.props.fetchBusiness(this.props.match.params.businessId)
+    }
+
+    renderHours(){
+        if(this.props.business.hours === undefined){
+            return null
+        }else{
+            return this.props.business.hours.map( (hour,idx) => <li key={idx}>{hour.date}   {hour.open} - {hour.close}</li>)
+        }
     }
 
     render(){
@@ -20,11 +30,20 @@ class BusinessDetail extends React.Component{
                 <div>
                     {business.photoUrls.map( (url,idx)=> <img src={url} key={idx} className='business-detail-img'/>)}
                 </div>
-               {business.name}
-               {business.address}
-               {business.city}
-               {business.website}
-               {business.phoneNumber}
+               <h1 className='business-detail-title'>{business.name}</h1>
+               <div className='business-detail-container'>
+                   <div>
+                       <p>Hours</p>
+                       <ul>
+                          {this.renderHours()}
+                       </ul>
+                   </div>
+                   <div className='business-detail-info'>
+                        <p>{business.address} {business.city},{business.state} {business.zipCode}</p>
+                        <a href={business.website}> Website <MdLaunch /> </a>
+                        <p>{business.phoneNumber} <BiPhoneCall /></p>
+                    </div>
+               </div>
             </div>
         )
 
