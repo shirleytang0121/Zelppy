ff90bba12e0700f374722c0baf4bacc60611aead
Shirley Tang <shirleytang1996@gmail.com>
2021-08-07 23:53:59 -0400

    finish rating in review form

--- frontend/components/review/review_form.jsx
+++ frontend/components/review/review_form.jsx
@@ -14,7 +14,7 @@ class ReviewForm extends React.Component{
         }
 
         this.handleSubmit = this.handleSubmit.bind(this)
-        
+        this.hoverLeave =this.hoverLeave.bind(this)
         // this.handleFile =this.handleFile.bind(this)
     }
 
@@ -51,23 +51,112 @@ class ReviewForm extends React.Component{
     }
      
     hoverStars(rating){
-        if(rating===1){
-
+        let star1 = document.getElementById("star1");
+        let star2 = document.getElementById("star2");
+        let star3 = document.getElementById("star3");
+        let star4 = document.getElementById("star4");
+        let star5 = document.getElementById("star5");
+        if(rating===0){
+            star1.className = 'star1-0';
+            star2.className = 'star2-0';
+            star3.className = 'star3-0';
+            star4.className = 'star4-0';
+            star5.className = 'star5-0';
+        }else if(rating===1){
+            star1.className = 'star1-1';
+            star2.className = 'star2-1';
+            star3.className = 'star3-1';
+            star4.className = 'star4-1';
+            star5.className = 'star5-1';
+        }else if(rating===2){
+            star1.className = 'star1-2';
+            star2.className = 'star2-2';
+            star3.className = 'star3-2';
+            star4.className = 'star4-2';
+            star5.className = 'star5-2';
+        }else if(rating===3){
+            star1.className = 'star1-3';
+            star2.className = 'star2-3';
+            star3.className = 'star3-3';
+            star4.className = 'star4-3';
+            star5.className = 'star5-3';
+        }else if(rating===4){
+            star1.className = 'star1-4';
+            star2.className = 'star2-4';
+            star3.className = 'star3-4';
+            star4.className = 'star4-4';
+            star5.className = 'star5-4';
+        }else if(rating===5){
+            star1.className = 'star1-5';
+            star2.className = 'star2-5';
+            star3.className = 'star3-5';
+            star4.className = 'star4-5';
+            star5.className = 'star5-5';
         }
     }
 
 
+    hoverLeave(e){
+        let star1 = document.getElementById("star1");
+        let star2 = document.getElementById("star2");
+        let star3 = document.getElementById("star3");
+        let star4 = document.getElementById("star4");
+        let star5 = document.getElementById("star5");
+        let rating = e.currentTarget.value
+        if(rating===0){
+            star1.className = 'star1-0';
+            star2.className = 'star2-0';
+            star3.className = 'star3-0';
+            star4.className = 'star4-0';
+            star5.className = 'star5-0';
+        }else if(rating===1){
+            star1.className = 'star1-1';
+            star2.className = 'star2-1';
+            star3.className = 'star3-1';
+            star4.className = 'star4-1';
+            star5.className = 'star5-1';
+        }else if(rating===2){
+            star1.className = 'star1-2';
+            star2.className = 'star2-2';
+            star3.className = 'star3-2';
+            star4.className = 'star4-2';
+            star5.className = 'star5-2';
+        }else if(rating===3){
+            star1.className = 'star1-3';
+            star2.className = 'star2-3';
+            star3.className = 'star3-3';
+            star4.className = 'star4-3';
+            star5.className = 'star5-3';
+        }else if(rating===4){
+            star1.className = 'star1-4';
+            star2.className = 'star2-4';
+            star3.className = 'star3-4';
+            star4.className = 'star4-4';
+            star5.className = 'star5-4';
+        }else if(rating===5){
+            star1.className = 'star1-5';
+            star2.className = 'star2-5';
+            star3.className = 'star3-5';
+            star4.className = 'star4-5';
+            star5.className = 'star5-5';
+        }
+
+    }
+
+    
     render(){
+        console.log(this.state)
         if(this.props.business === undefined) return null;
         return(
             <div >
                 <h1>{this.props.business.name}</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <input type="radio" value='1' onChange={this.update('rating')} name='rate' id="star1" onMouseEnter={()=>this.hoverStars(1)} onMouseLeave={()=>this.hoverStars(0)}/>
-                    <input type="radio" value='2' onChange={this.update('rating')} name='rate' id="star2" onMouseEnter={()=>this.hoverStars(2)} onMouseLeave={()=>this.hoverStars(0)}/>
-                    <input type="radio" value='3' onChange={this.update('rating')} name='rate' id="star3" onMouseEnter={()=>this.hoverStars(3)} onMouseLeave={()=>this.hoverStars(0)}/>
-                    <input type="radio" value='4' onChange={this.update('rating')} name='rate' id="star4" onMouseEnter={()=>this.hoverStars(4)} onMouseLeave={()=>this.hoverStars(0)}/>
-                    <input type="radio" value='5' onChange={this.update('rating')} name='rate' id="star5" onMouseEnter={()=>this.hoverStars(5)} onMouseLeave={()=>this.hoverStars(0)}/>
+                   
+                    <input type="radio" value='1' onChange={this.update('rating')} name='rate' id="star1" onMouseEnter={()=>this.hoverStars(1)} onMouseLeave={this.hoverLeave} />
+                    <input type="radio" value='2' onChange={this.update('rating')} name='rate' id="star2" onMouseEnter={()=>this.hoverStars(2)} onMouseLeave={this.hoverLeave} />
+                    <input type="radio" value='3' onChange={this.update('rating')} name='rate' id="star3" onMouseEnter={()=>this.hoverStars(3)} onMouseLeave={this.hoverLeave} />
+                    <input type="radio" value='4' onChange={this.update('rating')} name='rate' id="star4" onMouseEnter={()=>this.hoverStars(4)} onMouseLeave={this.hoverLeave} />
+                    <input type="radio" value='5' onChange={this.update('rating')} name='rate' id="star5" onMouseEnter={()=>this.hoverStars(5)} onMouseLeave={this.hoverLeave} />
                    
                     <select onChange={this.update('price_range')}>
                         <option value="$">$</option>
