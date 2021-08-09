4b82829db17566723791955f9b1a5948262c82c5
Shirley Tang <shirleytang1996@gmail.com>
2021-08-09 14:09:28 -0400

    add google map api

--- frontend/components/map/business_map.jsx
+++ frontend/components/map/business_map.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 
 class BusinessMap extends React.Component{
-    render(){
-        return(
-            <div id='map-container' ref='map'>
-             <p>map map map</p>
-            </div>
+    componentDidMount() {
+        const mapOptions = {
+          center: { lat: 40.75231, lng:  -73.98563  }, 
+          zoom: 13
+        };
+    
+        // wrap this.mapNode in a Google Map
+        this.map = new google.maps.Map(this.mapNode, mapOptions);
+      }
+    
+      render() {
+        return (
+          
+          <div id='map-container'  ref={ map => this.mapNode = map }> 
+          
+          </div>
         )
+      }
 
-    }
 }
 
 export default BusinessMap
