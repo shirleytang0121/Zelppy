5a53cbc47688e65aea62e205067b46c3b80c3a68
Shirley Tang <shirleytang1996@gmail.com>
2021-08-13 01:49:35 -0400

    fix some map errors

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -10,19 +10,19 @@ export default class MarkerManager {
         const businessObj ={}
         businesses.forEach(business => businessObj[business.id]=businesses)
         businesses.filter(business => !this.markers[business.id])
-                  .forEach(newBusiness => this.createMarkerFromBusiness(newBusiness))
+                  .forEach((newBusiness,idx) => this.createMarkerFromBusiness(newBusiness,idx))
 
         Object.keys(this.markers).filter(businessId => !businessObj[businessId])
                                  .forEach(businessId => this.removeMarker(this.markers[businessId]))
     }
 
-    createMarkerFromBusiness(business){
+    createMarkerFromBusiness(business,idx){
         const myLatLng = { lat: business.lat, lng: business.lng };
         const marker = new google.maps.Marker({
             position: myLatLng,
             map: this.map,
-            businessId: business.id
-
+            businessId: business.id,
+            label: (idx+1).toString() 
           });
 
           marker.addListener('click', () => this.handleClick(business));
