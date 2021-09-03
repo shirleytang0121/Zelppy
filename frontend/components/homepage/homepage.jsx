69b93a410d8a7c920e58d288ad301372313a9cdf
Shirley Tang <shirleytang1996@gmail.com>
2021-09-03 16:50:15 -0400

    add view business btn

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -11,11 +11,18 @@ class HomePage extends React.Component{
   constructor(props){
     super(props)
     this.handleClick =this.handleClick.bind(this)
+    this.handleDrink=this.handleDrink.bind(this)
 }
 
 handleClick(e){
     this.props.updateFilter('search',{value:'',position:''});
     this.navToBusinesses()
+}
+
+handleDrink(e){
+  e.preventDefault()
+  this.props.updateFilter('search',{value:'coffee and tea',position:''})
+  this.props.history.push('/search/coffee+and+tea')
 }
 
 navToBusinesses(){
@@ -31,6 +38,10 @@ navToBusinesses(){
             </header>
             <Link to='/'> <img src={window.logoURL} className='logo'/> </Link>
             <SearchBar updateFilter={this.props.updateFilter}  type='home-search'/>
+            <div className='nav-btn-div'>
+              <button onClick={this.handleClick} className='nav-btn'>Restaurants</button>
+              <button onClick={this.handleDrink} className='nav-btn'>Drink</button>
+            </div>
             <Navigation updateFilter={this.props.updateFilter}/>
             <Footer/>
           </div>
